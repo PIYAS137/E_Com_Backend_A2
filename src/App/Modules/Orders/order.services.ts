82c767e2aa616b64512ac0d3e2a83ea6b4155d73
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-05-21 16:12:38 +0600

    comment added

--- src/App/Modules/Orders/order.services.ts
+++ src/App/Modules/Orders/order.services.ts
@@ -4,16 +4,21 @@ import { Order_Model } from "./order.model";
 
 // Create Order Service
 const Create_Order_Service = async (newOrder: Order_Type) => {
-
+    // Get the available quantity
     const Available_Quantity: number | null = await Order_Model.getProduct_Quantity(newOrder.productId);
+
     if (Available_Quantity) {
         if (Available_Quantity < newOrder.quantity) {
+            // if avaiable quantity is lower then thee requested quantity
             throw new Error("Insufficient quantity available in inventory")
         } else {
+            // if avaiable quantity is greater then thee requested quantity & before place order it is update the product quantity
             const x = await Order_Model.updateProduct_Quantity(newOrder.productId, newOrder.quantity);
-            if (x == -1) { // if the product is modified after query and before the update !
+            // if the product is modified after query and before the update !(Internal DB Issue)
+            if (x == -1) { 
                 throw new Error("There is an server side error")
             } else {
+                // If every thing is ok then place order
                 const result = await Order_Model.create(newOrder);
                 return result;
             }
